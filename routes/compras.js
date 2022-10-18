c48474ec7cf7ef979959032569635c5431324360
Leonel Daruich <leodaruich2@gmail.com>
2022-10-18 02:20:04 -0300

    Corrigiendo rama

--- routes/compras.js
+++ routes/compras.js
@@ -1,22 +1,6 @@
 "use strict"
 const express = require("express")
 const router = express.Router()
-const productos = require("../stock.js")
-const d= document
-const contProd = d.getElementById('c-prod')
-
-productos.forEach(producto => {
-    const div = document.createElement('div')
-    div.innerHTML = `
-    <img src=${producto.img} alt="producto">
-    <p>${producto.nombre}</p>
-    <h3>${producto.precio}</h3>
-    `
-});
-
-
-
-
 
 router.get("/", (req,res)=>{
     res.render("compras")
