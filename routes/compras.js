90ea6b165e6607da318d6e03cc794bb4ca4cde5d
Leonel Daruich <leodaruich2@gmail.com>
2022-10-18 01:42:55 -0300

    Cambiando rama

--- routes/compras.js
+++ routes/compras.js
@@ -1,7 +1,18 @@
 "use strict"
 const express = require("express")
 const router = express.Router()
+const productos = require("../stock.js")
+const d= document
+const contProd = d.getElementById('c-prod')
 
+productos.forEach(producto => {
+    const div = document.createElement('div')
+    div.innerHTML = `
+    <img src=${producto.img} alt="producto">
+    <p>${producto.nombre}</p>
+    <h3>${producto.precio}</h3>
+    `
+});
 
 
 
